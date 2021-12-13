8d61939e54dca70e8be2338e2db7d1affd706669
Satoshi <merapano@gmail.com>
2021-12-13 20:36:37 +0900

    Create and switch to a new buffer ("*recoll*")
      and assign "\C-c\C-y" to the function "recoll-query"

--- recoll.el
+++ recoll.el
@@ -1,5 +1,7 @@
 (defun recoll-query ()
   (interactive "*"  )
+  (generate-new-buffer "*recoll*")
+  (switch-to-buffer "*recoll*")
   (let* ((qstring (read-string "Query: "))
          (command (concat "~/usr/recoll-utils/example.py \"" qstring "\" > ~/tmp.html" )))
 
@@ -7,6 +9,9 @@
     (shell-command command)
     (w3m-find-file "~/tmp.html")
     ))
+
+(global-set-key "\C-c\C-y" 'recoll-query)
+
 
 
 
