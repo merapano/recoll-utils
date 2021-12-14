0ea9b3e73616fc86ad56d6e32c057051de23fa05
Satoshi <merapano@gmail.com>
2021-12-14 17:13:00 +0900

    On Emsworth (Mint on Intel NUC)

--- recoll.el
+++ recoll.el
@@ -1,16 +1,21 @@
 (defun recoll-query ()
   (interactive "*"  )
-  (generate-new-buffer "*recoll*")
-  (switch-to-buffer "*recoll*")
-  (let* ((qstring (read-string "Query: "))
-         (command (concat "~/usr/recoll-utils/example.py \"" qstring "\" > ~/tmp.html" )))
-
+  (let* ((tmpbuffer   (generate-new-buffer "*recoll*"))
+         (qstring (read-string "Query: "))
+         (command
+          (concat
+           "~/usr/recoll-utils/example.py \""
+           qstring
+           "\" > ~/tmp.html" ))
 ;;;    (shell-command-to-string command)
+         )
+    (switch-to-buffer tmpbuffer)
+    (set-buffer tmpbuffer)
     (shell-command command)
-    (w3m-find-file "~/tmp.html")
-    ))
+    (w3m-find-file "~/tmp.html")))
 
 (global-set-key "\C-c\C-y" 'recoll-query)
+
 
 
 
